78151eaf2f4ed73f31c0c574c7ee650bac8cb994
Imam Fauzan <imamfzn@gmail.com>
2021-02-13 19:59:06 +0700

    fix mongoclient warning

--- src/app.js
+++ src/app.js
@@ -5,8 +5,6 @@ const mongoose = require('mongoose');
 const userRouter = require('./routes/user');
 const middleware = require('./middlewares');
 
-const app = express();
-
 if (!process.env.ACCESS_TOKEN_SECRET){
   console.error("ERROR: ACCESS_TOKEN_SECRET not provided!");
   process.exit(1);
@@ -17,14 +15,27 @@ if (!(process.env.AUTAN_BASIC_USER && process.env.AUTAN_BASIC_PASSWORD)){
   process.exit(1);
 }
 
-mongoose
-  .connect("mongodb://localhost/insan", { useNewUrlParser: true })
-  .then(() => console.log("Connected to mongodb."))
-  .catch(err => console.error(`Can't connect to mongodb. Error:\n${err}`));
+const app = express();
 
 app.use(express.json());
 app.use('/users', userRouter);
 app.use(middleware.error);
 
+(
+  async function (){
+    try {
+      await mongoose.connect(
+        'mongodb://localhost/insan',{
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useFindAndModify: false,
+          useCreateIndex: true
+        }
+      );
+    } catch (e) {
+      throw e;
+    }
 
-app.listen(3001, () => console.log("App is running"));
+    app.listen(3001, () => console.log("Insan is running on port 3001."));
+  }
+)();
