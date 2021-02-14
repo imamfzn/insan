456eb59dfb9faaef66b21a2b6c212e62443b0434
Imam Fauzan <imamfzn@gmail.com>
2021-02-14 23:44:26 +0700

    using snake_case for timestamp

--- src/models/user.js
+++ src/models/user.js
@@ -25,6 +25,11 @@ const User = new mongoose.Schema({
     default: null,
   },
 },
-{ timestamps: true });
+{
+  timestamps: {
+    createdAt: 'created_at',
+    updatedAt: 'updated_at',
+  },
+});
 
 module.exports = mongoose.model('User', User);
