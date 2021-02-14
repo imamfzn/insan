3652fc611cebf19d617da06df19b87981587088e
Imam Fauzan <imamfzn@gmail.com>
2021-02-14 07:25:34 +0700

    fix indent

--- src/services/user/create.js
+++ src/services/user/create.js
@@ -2,24 +2,24 @@ const User = require('../../models/user');
 const Autan = require('../../connections/autan');
 
 async function create(user){
-	const { username, password, role } = user;
+  const { username, password, role } = user;
 
-	try {
-		const userAuth = await Autan.register({username, password, role});
-		const userCreate = new User({
-		  auth_id: userAuth._id,
-		  name: user.name,
-		  email: user.email,
-		  address: user.address,
-		  phone: user.phone,
-		});
+  try {
+    const userAuth = await Autan.register({username, password, role});
+    const userCreate = new User({
+      auth_id: userAuth._id,
+      name: user.name,
+      email: user.email,
+      address: user.address,
+      phone: user.phone,
+    });
 
-		await userCreate.save();
+    await userCreate.save();
 
-		return {...userAuth, ...userCreate.toObject() };
-	} catch (err) {
-		throw err;
-	}
+    return {...userAuth, ...userCreate.toObject() };
+  } catch (err) {
+    throw err;
+  }
 }
 
 module.exports = create;
