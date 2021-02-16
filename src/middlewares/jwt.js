0cd5231589b28e2a5d82a2f417e3f4092b44410e
Imam Fauzan <imamfzn@gmail.com>
2021-02-16 10:51:25 +0700

    using bearer token

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,11 +1,3 @@
 const jwt = require('express-jwt');
 
-function getToken(req) {
-  return req.headers['x-access-token'] || req.headers.authorization;
-}
-
-module.exports = jwt({
-  secret: process.env.ACCESS_TOKEN_SECRET,
-  algorithms: ['HS256'],
-  getToken,
-});
+module.exports = jwt({ secret: process.env.ACCESS_TOKEN_SECRET, algorithms: ['HS256'] });
